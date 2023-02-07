5465b3311b25bcb764690616382f003ea920adf5
Jan-Philipp Kempf <jan-philipp.kempf@mindcurv.com>
2023-02-07 12:38:36 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import React, { useRef, useEffect } from 'react';
-import WebViewer from '@pdftron/webviewer';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import WebViewer from "@pdftron/webviewer";
+
+import xfdfStrings from "./data";
+
+import "./App.css";
 
 const App = () => {
   const viewer = useRef(null);
 
-  // if using a class, equivalent of componentDidMount 
+  // if using a class, equivalent of componentDidMount
   useEffect(() => {
     WebViewer(
       {
-        path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
+        path: "/webviewer/lib",
+        initialDoc: "/files/plan.pdf",
       },
-      viewer.current,
+      viewer.current
     ).then((instance) => {
       const { documentViewer, annotationManager, Annotations } = instance.Core;
 
-      documentViewer.addEventListener('documentLoaded', () => {
-        const rectangleAnnot = new Annotations.RectangleAnnotation({
-          PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
-          X: 100,
-          Y: 150,
-          Width: 200,
-          Height: 50,
-          Author: annotationManager.getCurrentUser()
-        });
+      console.log(xfdfStrings);
 
-        annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
-        annotationManager.redrawAnnotation(rectangleAnnot);
+      documentViewer.addEventListener("documentLoaded", () => {
+        xfdfStrings.map((xfdfString) =>
+          annotationManager.importAnnotations(xfdfString)
+        );
       });
     });
   }, []);
