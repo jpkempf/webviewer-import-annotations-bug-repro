6f8981e4b40ecf01826065e996b1e2a442eebd45
Jan-Philipp Kempf <jan-philipp.kempf@mindcurv.com>
2023-02-14 10:28:00 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -20,14 +20,25 @@ const App = () => {
       const { documentViewer, annotationManager } = instance.Core;
 
       documentViewer.addEventListener("documentLoaded", async () => {
-        // option 1
+        // option 1; loop over the list of xfdf strings
+
         // xfdfStrings.forEach(
         //   async (xfdfString) =>
         //     await annotationManager.importAnnotations(xfdfString)
         // );
 
-        // option 2
-        await annotationManager.importAnnotations(combinedXfdfString);
+        // option 2; draw one giant combined xfdf string
+
+        // await annotationManager.importAnnotations(combinedXfdfString);
+
+        /*
+         * option 3; draw a single annotation. any single one will look fine,
+         * but drawing more than one leads to the same issue as before.
+         */
+
+        // annotationManager.importAnnotations(xfdfStrings[0]);
+        // annotationManager.importAnnotations(xfdfStrings[1]);
+        annotationManager.importAnnotations(xfdfStrings[2]);
       });
     });
   }, []);
